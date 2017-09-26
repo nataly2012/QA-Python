0f3918ea01957950d2a84f94e1d5e08824fb2102
unknown <bna@LT-214210.kyiv.ciklum.net>
2017-09-26 12:30:05 +0300

    Initial commit

---
+++ Lesson1.py
@@ -0,0 +1,46 @@
+import math
+
+#Task 1.1
+#
+# string=input("Print something here:")
+# if string.isdigit():
+#     print("You " + str(string) + " has numbers")
+# else:
+#    print("You " + str(string) + " has letters")
+
+#Task 1.4
+# text="Homework"
+# print(text.center(100, ' '))
+
+#Task 1.5
+# text2 = (input("Paste text here:"))
+# print(text2.title())
+
+#Task 2.5
+# num = float(input("Print float number:"))
+# print(num - int(num))
+
+#Task 2.6
+# num = float(input("Print float number:"))
+# n=int(num*10)%10
+# print(n)
+
+#Task3
+# year = input("Please type your year of birth here:")
+# year = int(year)
+# if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+#     print("YES, you birth year is a leap-year")
+# else:
+#     print("NO, you birth year is not a leap-year")
+
+#Task4
+# a=input("a=")
+# a=int(a)
+# b=input("b=")
+# b=int(b)
+# c=input("c=")
+# c=int(c)
+# if a + b > c and a + c > b and b + c > a:
+# 	print("Yes, triangle exists ")
+# else:
+# 	print("No, triangle does not exist")
