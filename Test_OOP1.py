b06652c41fc0682343845377fb5b4ef64e8d6201
nataly2012 <32015727+nataly2012@users.noreply.github.com>
2017-10-28 15:42:37 +0300

    Delete Test_OOP1.py
    delete

--- Test_OOP1.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from OOP1 import  ITEmpl
-class EmplTest(unittest.TestCase):
-    def setUp(self):
-        self.emp = ITEmpl("Nata", "Pin")
-
-    def test_name(self):
-        self.assertEqual (self.emp.name, "Nata")
-        self.assertEqual (self.emp.surname, "Pin")
